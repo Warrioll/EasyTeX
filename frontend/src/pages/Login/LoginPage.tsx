add11893e49c445c28d995059282fe027904eb8b
Karol Nowak <karnowa88@gmail.com>
2024-12-09 06:04:16 +0100

    login page bg

--- frontend/src/pages/Login/LoginPage.tsx
+++ frontend/src/pages/Login/LoginPage.tsx
@@ -1,5 +1,7 @@
 import {
   Anchor,
+  BackgroundImage,
+  Box,
   Button,
   Checkbox,
   Container,
@@ -11,46 +13,44 @@ import {
   Text,
   TextInput,
   Title,
-  BackgroundImage} from '@mantine/core';
+} from '@mantine/core';
 import styles from './loginPage.module.css';
 
-
 export default function LoginPage() {
-  return (<>
-   <BackgroundImage
-        src="./latex-bg1.png"
-        radius="xs"
-       
-      >
-    <Stack h="100vh" align="stretch" justify="center">
-      <Container w={420} mb="6%">
-       
+  return (
+    // 14 lub 13
+    <>
+      <BackgroundImage src="./bg13.png" radius="xs">
+        <Stack h="100vh" align="stretch" justify="center">
+          <Container w={420} mb="6%">
+            <Paper withBorder shadow="xl" p={30} mt={30} radius="md">
+              <Title ta="center" className={styles.title}>
+                Welcome back!
+              </Title>
+              <Text c="dimmed" size="sm" ta="center" mt={5} mb={20}>
+                Do not have an account yet?{' '}
+                <Anchor size="sm" href="/register" target="_blank">
+                  Create account
+                </Anchor>
+              </Text>
 
-        <Paper withBorder shadow="md" p={30} mt={30} radius="md" className={styles.paper}>
-        <Title ta="center" className={styles.title}>
-          Welcome back!
-        </Title>
-        <Text c="dimmed" size="sm" ta="center" mt={5} mb={20}>
-          Do not have an account yet?{' '}
-          <Anchor size="sm"href="/register" target="_blank">
-            Create account
-          </Anchor>
-        </Text>
-          <TextInput label="Email" placeholder="you@mantine.dev" required />
-          <PasswordInput label="Password" placeholder="Your password" required mt="md" />
-          <Group justify="space-between" mt="lg">
-            {/* <Checkbox label="Remember me" />
+              <Box className={styles.paper}>
+                <TextInput label="Email" placeholder="you@mantine.dev" required />
+                <PasswordInput label="Password" placeholder="Your password" required mt="md" />
+              </Box>
+              <Group justify="space-between" mt="lg">
+                {/* <Checkbox label="Remember me" />
           <Anchor component="button" size="sm">
             Forgot password?
           </Anchor> */}
-          </Group>
-          <Button fullWidth mt="xl" >
-            Sign in
-          </Button>
-        </Paper>
-      </Container>
-    </Stack>
-    </BackgroundImage>
+              </Group>
+              <Button fullWidth mt="xl">
+                Sign in
+              </Button>
+            </Paper>
+          </Container>
+        </Stack>
+      </BackgroundImage>
     </>
   );
 }
